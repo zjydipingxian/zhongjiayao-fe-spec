bff48cb9f6256e4984a0334ba625bd18433abe27
Null <7566349+zhongjiayao@user.noreply.gitee.com>
2024-08-26 21:29:24 +0800

    feat(eslint-plugin): 更新推荐配置中的规则别名
    - 将`encode-fe-eslint-plugin`别名为`@zhongjiayao/eslint-plugin`，以保持一致性。
    - 确保`no-secret-info`规则现在引用更新后的插件别名。

--- packages/eslint-plugin/configs/recommended.js
+++ packages/eslint-plugin/configs/recommended.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: ['@zhongjiayao/eslint-plugin'],
   rules: {
     '@zhongjiayao/eslint-plugin/no-http-url': 'warn',
-    'encode-fe-eslint-plugin/no-secret-info': 'error',
+    '@zhongjiayao/eslint-plugin/no-secret-info': 'error',
   },
 }
